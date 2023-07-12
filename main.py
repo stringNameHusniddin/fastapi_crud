40152a3abac9e14be10813623a62663a8b44d1ce
stringNameHusniddin <stringhusniddin@gmail.com>
2023-07-12 20:56:02 +0500

    crud fastapi

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, Depends
+import models, database, schemas
+from sqlalchemy.orm import Session
+from database import SessionLocal
+
+models.Base.metadata.create_all(bind=database.engine)
+
+app = FastAPI()
+
+def getdb():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/blog")
+def list(db:Session=Depends(getdb)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get("/blog/{id}")
+def detail(id:int, db:Session=Depends(getdb)):
+    blog = db.query(models.Blog).filter(models.Blog.id ==id).first()
+    return blog
+@app.post("/blog")
+def create(req: schemas.Blog, db : Session = Depends(getdb)):
+    new_blog = models.Blog(title=req.title, body=req.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+@app.put("/blog/{id}")
+def update(id:int, req:schemas.Blog, db:Session=Depends(getdb)):
+    db.query(models.Blog).filter(models.Blog.id == id).update(req.dict())
+    db.commit()
+    return "update"
+
+@app.delete("/blog/{id}")
+def destroy(id:int, db:Session=Depends(getdb)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return "done"
---
+++ models.py
@@ -0,0 +1,9 @@
+from database import Base
+from sqlalchemy import String, Integer, Column
+
+class Blog(Base):
+    __tablename__ = "blogs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    body = Column(String)
